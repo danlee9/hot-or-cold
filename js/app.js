1f324f38999a46c0ebd3d4cf6def4ae69527f1e1
unknown <dan.dh.lee@gmail.com>
2015-03-30 22:59:48 -0700

    Add game won message and functionality

--- js/app.js
+++ js/app.js
@@ -12,9 +12,7 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	var num, count, guess, guessDif;
-
-  	count = 0;
+  	var num, count, guess, guessDif, gameOver;
 
   	var $guess = $('#userGuess'),
   		$feedback = $('#feedback'),
@@ -30,14 +28,14 @@ $(document).ready(function(){
 
   	$("form").submit(function(event) {
   		event.preventDefault();
-  		addCount();
-  		addList();
   		feedback();
   	});
 
   	function newGame() {
-  		$guess.val('');
-  		count = 0;
+  		gameOver = false;
+      count = 0;
+      $feedback.text('Make your Guess!');
+      $guess.val('');
   		$count.text(count);
   		$guessList.empty();
   		num = Math.floor(Math.random() * 100) + 1;
@@ -45,31 +43,38 @@ $(document).ready(function(){
   	}
 
   	function addCount() {
-  		count++;
-  		$count.text(count);
+        count++;
+        $count.text(count);
   	}
 
   	function addList() {
-  		guess = $guess.val();
-  		$guessList.append('<li>' + guess + '</li>');
+        guess = $guess.val();
+        $guessList.append('<li>' + guess + '</li>');
   	}
 
   	function feedback() {
-  		guessDif = Math.abs(guess-num);
-  		if (guessDif >= 50) {
-  			$feedback.text("Ice Cold");
-  		} else if (guessDif >= 30) {
-  			$feedback.text('Cold');
-  		} else if (guessDif >= 20) {
-  			$feedback.text("Warm");
-  		} else if (guessDif >= 10) {
-  			$feedback.text('Hot');
-  		} else if (guessDif > 0) {
-  			$feedback.text("Very Hot");
-  		} else {
-  			$feedback.text('You got it!');
-  		}
-  		$guess.val('');
+      if (!gameOver) {
+        addCount();
+        addList();
+        guessDif = Math.abs(guess-num);
+        if (guessDif >= 50) {
+          $feedback.text("Ice Cold");
+        } else if (guessDif >= 30) {
+          $feedback.text('Cold');
+        } else if (guessDif >= 20) {
+          $feedback.text("Warm");
+        } else if (guessDif >= 10) {
+          $feedback.text('Hot');
+        } else if (guessDif > 0) {
+          $feedback.text("Very Hot");
+        } else {
+          $feedback.text('You got it!');
+          gameOver = true;
+        }
+      } else {
+        $feedback.text("You've already won! Press New Game to start over.");
+      }
+      $guess.val('');
   	}
 
 
